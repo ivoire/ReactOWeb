6dfc9df2579687321500ad94a6b1ad141134f952
Rémi Duraffort <ivoire@videolan.org>
2016-05-27 15:41:18 +0200

    Model: import the Message class

--- ReactOWeb/models.py
+++ ReactOWeb/models.py
@@ -1,3 +1,23 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    """
+    This class is the exact clone of the one in ReactOBus.lib.db
+    Assuming that this class is for Django while the other one is for
+    SQLAlchemy.
+    """
+    topic = models.TextField(db_index=True)
+    uuid = models.TextField(db_index=True)
+    datetime = models.DateTimeField(db_index=True)
+    username = models.TextField(db_index=True)
+    data = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'messages'
+
+    def __str__(self):
+        return "%s %s (%s)" % (self.topic, self.datetime, self.username)
