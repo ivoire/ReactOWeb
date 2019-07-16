375da12601a4d6c4267ee4fce5c4d8e0c04907de
Rémi Duraffort <remi.duraffort@linaro.org>
2019-07-16 17:24:19 +0200

    Remove unused import
    Found by pylint.
    
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

--- ReactOWeb/urls.py
+++ ReactOWeb/urls.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with ReactOWeb.  If not, see <http://www.gnu.org/licenses/>
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 import ReactOWeb.views as v
 from ReactOWeb.feeds import MessageFeed
